85792fb5e2338c48be749674de76b72477484ca7
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-11 16:02:43 +0300

    feat: StockSlice Created by Mucahit

--- src/features/authSlice.jsx
+++ src/features/authSlice.jsx
@@ -1,14 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  user: "",
+  loading: false,
+  error: false,
+  token: "",
+};
+
 const authSlice = createSlice({
   name: "auth",
-
-  initialState: {},
-  reducers: {},
+  initialState,
+  reducers: {
+    fetchStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, { payload }) => {
+      state.loading = false;
+      state.user = payload.user.username;
+      state.token = payload.token;
+    },
+    registerSucces: (state, { payload }) => {
+      console.log(payload);
+      state.loading = false;
+      state.user = payload.data.username;
+      state.token = payload.token;
+    },
+    logoutSuccess: (state) => {
+      state.user = "";
+      state.loading = false;
+      state.token = "";
+    },
+    fetchFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+  },
 });
-// deneme
-//deneme-2
-//deneme-3
 
-export const {} = authSlice.actions;
+export const {
+  fetchStart,
+  fetchFail,
+  loginSuccess,
+  registerSucces,
+  logoutSuccess,
+} = authSlice.actions;
 export default authSlice.reducer;
